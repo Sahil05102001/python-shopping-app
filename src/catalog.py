5f24e0d06f50b63ef6d81e2856e2e55789e217ae
Sahil <ssk92216@gmail.com>
2026-08-22 14:11:39 +0530

    feat: add product catalog and categories

---
+++ src/catalog.py
@@ -0,0 +1,111 @@
+categories = [
+    {
+        "category_id": 1,
+        "name": "Boots"
+    },
+    {
+        "category_id": 2,
+        "name": "Coats"
+    },
+    {
+        "category_id": 3,
+        "name": "Jackets"
+    },
+    {
+        "category_id": 4,
+        "name": "Caps"
+    }
+]
+
+products = [
+    {
+        "product_id": 1,
+        "name": "Hiking Boots",
+        "category_id": 1,
+        "price": 2200,
+        "stock": 10
+    },
+    {
+        "product_id": 2,
+        "name": "Leather Boots",
+        "category_id": 1,
+        "price": 3000,
+        "stock": 10
+    },
+    {
+        "product_id": 3,
+        "name": "Work Boots",
+        "category_id": 1,
+        "price": 1750,
+        "stock": 10
+    },
+    {
+        "product_id": 4,
+        "name": "Winter Coat",
+        "category_id": 2,
+        "price": 2500,
+        "stock": 10
+    },
+    {
+        "product_id": 5,
+        "name": "Wool Coat",
+        "category_id": 2,
+        "price": 4000,
+        "stock": 10
+    },
+    {
+        "product_id": 6,
+        "name": "Rain Coat",
+        "category_id": 2,
+        "price": 1000,
+        "stock": 10
+    },
+    {
+        "product_id": 7,
+        "name": "Denim Jacket",
+        "category_id": 3,
+        "price": 1800,
+        "stock": 10
+    },
+    {
+        "product_id": 8,
+        "name": "Leather Jacket",
+        "category_id": 3,
+        "price": 4500,
+        "stock": 10
+    },
+    {
+        "product_id": 9,
+        "name": "Bomber Jacket",
+        "category_id": 3,
+        "price": 2000,
+        "stock": 10
+    },
+    {
+        "product_id": 10,
+        "name": "Trucker Cap",
+        "category_id": 4,
+        "price": 400,
+        "stock": 10
+    },
+    {
+        "product_id": 11,
+        "name": "Baseball Cap",
+        "category_id": 4,
+        "price": 350,
+        "stock": 10
+    },
+    {
+        "product_id": 12,
+        "name": "Dad Cap",
+        "category_id": 4,
+        "price": 450,
+        "stock": 10
+    }
+]
+
+def get_products():
+    return products
+
+def get_categories():
+    return categories
